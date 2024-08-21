593d82373e7932b95625798dd2a2fdcff838a396
‘angeynik’ <angeynik@gmail.com>
2024-08-21 12:35:00 +0300

    Преобразовано базовое приложение App.vue добадены работа с websocket

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,16 +1,46 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>"Это основная страница приложения пользовательского приложения управления Умным домом !!! "</h1>
+<hr />
+<SetParams />
+<button v-on:click="sendMessage ('Тестируем соединение')"> Send Message </button>
+<hr />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import { createServer } from 'http';
+import { WebSocket } from 'ws';
+
+import SetParams from './components/SetParams.vue';
+// onst wss = new WebSocketServer({ port: 9200 });
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    SetParams
+  },
+  data: function () {
+    return {
+      connection: null
+    }
+  },
+  created: function () {
+    console.log("Started Connection to WebSocket Node_RED");
+    this.connection = new WebSocket("ws://129.47.1.60:1880/dashboard");
+
+    this.connection.onopen = function (event) {
+      console.log("Connection established", event);
+    }
+
+    this.connection.onmessage = function (event) {
+      console.log("Received: %s", event.data);
+    }
+  },
+  methods: {  
+    sendMessage: function (message) {
+      this.connection.send("Hello, Node-RED !", message);
+    }
+  },
 }
 </script>
 
---
+++ vue-app/src/components/SetParams.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>SetParams</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
