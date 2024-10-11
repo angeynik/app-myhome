935b13ef23a0bce21404b3fd19ba577343900f10
‘angeynik’ <angeynik@gmail.com>
2024-10-11 12:26:28 +0300

    Реализован механизм отображения svg иконок

--- vue-app/src/components/BodySetpontBlock.vue
+++ vue-app/src/components/BodySetpontBlock.vue
@@ -3,12 +3,18 @@
     <div>
       <h2> {{ lowLimit }} </h2>
     </div>
+    <div class="bodySetpoint_selector">
+
+    </div>
     <div
     @touchstart="handleTouchStart"
     @touchend="handleTouchEnd" 
     @touchmove="handleTouchMove"
     >
       <h1> {{ setPoint }} </h1>
+    </div>
+    <div style="background-image: url(../public/img/bodySetpoint_selector.svg);">
+
     </div>
     <div>
       <h2> {{ highLimit }} </h2>
